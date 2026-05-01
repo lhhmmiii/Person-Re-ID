fcd89a4be294c788b500d0ae2d5cb5ba8098e35c
lhhmmiii <lehuuhung30023010@gmail.com>
2026-05-01 15:49:49 +0700

    chore: edit output dir

--- external/ByteTrack/yolox/exp/base_exp.py
+++ external/ByteTrack/yolox/exp/base_exp.py
@@ -19,7 +19,7 @@ class BaseExp(metaclass=ABCMeta):
 
     def __init__(self):
         self.seed = None
-        self.output_dir = "./YOLOX_outputs"
+        self.output_dir = "../results"
         self.print_interval = 100
         self.eval_interval = 10
 
